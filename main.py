e7c079bdbe7fcbebbacd5015e3ae2d2be03db8ec
stickman-dev <50963269+stickman-dev@users.noreply.github.com>
2025-03-23 17:21:36 +0000

    add rotate

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
